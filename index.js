a7461396a9a0e2edcbdc68796ae9fa514208eee6
Haley Someone <hbarlar1@gmail.com>
2018-07-15 22:33:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function currentLine(katzDeliLine) {
 var newArray = [];
 
 for (let i = 1; i < katzDeliLine.length; i++){
-  newArray.push(" " + i + ". " + katzDeliLine[i]);
+  newArray.push(" " + (i + 1) + ". " + katzDeliLine[i]);
 }
 if (katzDeliLine.length === 0) {
   return "The line is currently empty.";
