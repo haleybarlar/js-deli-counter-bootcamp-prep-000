88eef7350466dca31006fa6c4a8e4de92fd1dcdf
Haley Someone <hbarlar1@gmail.com>
2018-07-15 22:31:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,5 +27,5 @@ for (let i = 0; i < katzDeliLine.length; i++){
 if (katzDeliLine.length === 0) {
   return "The line is currently empty.";
 }
-return "newArray";
+return "This line is currently: " + newArray;
 }
