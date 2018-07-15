25cd4af9ae24cc39f1f9a76fce04089ef164bcf0
Haley Someone <hbarlar1@gmail.com>
2018-07-15 22:22:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function currentLine(katzDeliLine) {
 var newArray = [];
 
 for (let i = 0; i < katzDeliLine.length; i++){
-  newArray.push((i + 1) + katzDeliLine[i]);
+  newArray.push((i + 1) + ". " + katzDeliLine[i]);
 }
 if (katzDeliLine.length === 0) {
   return "The line is currently empty.";
