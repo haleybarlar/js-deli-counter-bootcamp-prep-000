303af32fbcd7a99e72591ee09c5cd244cf7ea535
Haley Someone <hbarlar1@gmail.com>
2018-07-15 22:12:35 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,28 @@
+var katzDeliLine = ["Bill", "Jane", "Anne"];
+
+function takeANumber(katzDeliLine, name) {
+
+katzDeliLine.push(name);
+
+var number = katzDeliLine.length;
+
+return "Welcome, " + name + ". You are number " + number + " in line.";
+}
+
+
+function nowServing(katzDeliLine) {
+
+if (katzDeliLine.length === 0) {
+  return "There is nobody waiting to be served!"
+}
+return "Currently serving " + katzDeliLine.shift() + ".";
+}
+
+function currentLine(katzDeliLine) {
+  var number = [1, 2, 3, 4, 5,];
+  
+if (katzDeliLine.length === 0) {
+  return "The line is currently empty.";
+}
+return "The line is currently: " +  katzDeliLine;
+}
